6e21bb41235b067d6ec45dc741d783c1f02cf2f6
Razin Farhan Hussain <razin2good@gmail.com>
2025-02-20 15:53:57 -0500

    First Commit

---
+++ seleniumAutomation/src/seleniumAutomation/StaticDropDown.java
@@ -0,0 +1,34 @@
+package seleniumAutomation;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class StaticDropDown {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		//dropdown with select tag;
+		WebElement staticDropdown = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
+		
+		Select dropdown = new Select(staticDropdown);
+		
+		dropdown.selectByIndex(3);//select USD 
+		
+		String currencyUSDTxt =  dropdown.getFirstSelectedOption().getText();
+		
+		System.out.println("Currency option selected: "+currencyUSDTxt);
+
+	}
+
+}
